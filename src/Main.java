de4fc281a3f56f86cc9f1a7e57aa2e57011943e0
AngelaBruh <angelabruna836@gmail.com>
2025-02-28 18:54:03 -0300

    desafio

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,35 @@
+import java.util.Scanner;
 
 public class Main {
+// Regra:Só pode transferir para o mesmo banco
+
+
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Scanner leitor = new Scanner(System.in);
+		Cliente cliente = new Cliente();
+		System.out.println("Informe o nome do titular:");
+		String titular = leitor.nextLine();
+		cliente.setNome(titular);
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
+		System.out.println("Informe um valor para saque:");
+		double saque = leitor.nextDouble();
+		cc.sacar(saque);
+
+		System.out.println("Informe o valor para deposito: ");
+		double deposito = leitor.nextDouble();
+		cc.depositar(deposito);
+
+		System.out.println("Informe o valor para transferência: ");
+		double transfere = leitor.nextDouble();
+		cc.transferir(transfere, poupanca);
 		
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
-	}
 
+		leitor.close();
+	}
 }
